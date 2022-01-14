f9a4968b49eddb9f09bd640ed0367fc8d05dca47
pinky1024t <pinky1024t@gmail.com>
2022-01-14 19:16:01 +0900

    Delete deploy.sh

--- bash/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-## Deploy Bash RCfiles to Home Directory
-ln -s $XDG_CONFIG_HOME/bash/.bash* -t $HOME/
--- zsh/deploy.zsh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-## Deploy .zshenv to Home Directory
-ln -s $XDG_CONFIG_HOME/zsh/.zshenv -t $HOME/
